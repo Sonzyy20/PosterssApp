d422a35b785d5eced316dff1c822093c5ace8b69
timmi <timajaf2005@gmail.com>
2024-10-27 23:22:15 +0100

    local fixes

---
+++ components/ThreadsCards.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+
+const props = defineProps({
+    post:{
+
+        type: Object,
+        required: true
+    }
+})
+
+
+
+</script>
+
+<template>
+  <div>
+    <h4>{{ post.title }}</h4>
+    <h1>{{ post.body }}</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,17 @@ import { UseAuthStore } from '~/store/autchSotre';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from '~/app.vue';
+import ThreadsCards from '~/components/ThreadsCards.vue';
 
 
+
+const posts = ref()
+
+onMounted(async() => {
+  const result = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const data = result.json()
+  posts.value = data;
+})
 
 const pinia = createPinia()
 const app = createApp(App)
@@ -33,26 +42,33 @@ console.log(name)
 </script>
 
 <template>
-  <div class="flex items-center justify-between w-full mt-2">
+  <div class="shaorma">
 
-    <UiButton type="button" variant="destructive" class="btn-left ml-5 px-4 py-2"  @click="logout">
+    <div class="flex items-center justify-between w-full mt-2">
+      
+      <UiButton type="button" variant="destructive" class="btn-left ml-5 px-4 py-2"  @click="logout">
         Logout
       </UiButton>
-
+      
       <div class="flex-grow"></div>
-
-    <div class="flex-grow">
-      <UiInput class="w-full max-w-sm px-4 py-2 border rounded-lg" placeholder="Searching"></UiInput>
-    </div>
-     
-
-    
-    <div class="flex mr-8 ml-0">
+      
+      <div class="flex-grow">
+        <UiInput class="w-full max-w-sm px-4 py-2 border rounded-lg" placeholder="Searching"></UiInput>
+      </div>
+      
+      
+      
+      <div class="flex mr-8 ml-0">
         <UiButton variant="primary" class="btn-right text-black px-4 py-4 w-24 border-solid border-2 p-2 border-black mr-3">Create Post</UiButton>
-
+        
         <UiButton variant="primary" class="btn-right text-black px-4 py-4 w-24 border-solid border-2 p-2 border-black">Hello: {{ AStore.myName }}</UiButton>
       </div>
     </div>
+    <div>
+      <ThreadsCards v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+    
+  </div>
   
 </template>
 
