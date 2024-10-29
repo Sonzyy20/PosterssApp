8ac91527693bd5689d99239b1456b99632fc0987
тимофей кабыш <timajaf2005@gmail.com>
2024-10-29 11:10:13 +0100

    adding-authors

--- components/PostTredsCards.vue
+++ components/PostTredsCards.vue
@@ -1,5 +1,7 @@
 
 <script lang="ts" setup>
+import { User } from 'lucide-vue-next';
+
 
 const props = defineProps({
   post: {
@@ -9,15 +11,21 @@ const props = defineProps({
 })
 
 
-
+const results = await fetch(`https://jsonplaceholder.typicode.com/users/${props.post.userId}`)
+const data = await results.json()
+const user = data
 </script>
 
 <template>
   <div class="PostCards border  rounded-xl border-black w-5/6 text-center mb-2 py-2 px-2">
     <div class="flex flex-col items-center" v-if="post">
-      <h4 class="text-2xl font-bold mb-1">{{ post.title }}</h4>
+      <h4 class="text-2xl font-bold mb-1">{{ post.title }} </h4>
       <div class="min-w-[700px] border border-gray-200"></div>
       <h1>{{ post.body }}</h1>
+      <div class="w-full flex justify-end">
+        <p class="text-slate-400">Author:</p>
+        <p class="text-slate-800">{{ `  ${user.name}` }}</p>
+      </div>
     </div>
     
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,15 @@ import App from '~/app.vue';
 
 
 const posts = ref([])
+const users = ref([])
 
 onMounted(async() =>{
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/")
   const data = await response.json()
   posts.value = data
+//   const results = await fetch(`https://jsonplaceholder.typicode.com/users/`)
+// const data2 = await results.json()
+//   users.value = data2
 })
 
 
@@ -43,37 +47,33 @@ console.log(name)
 </script>
 
 <template>
-  <div class="flex items-center justify-between w-full mt-2">
+  <div class="flex items-center justify-between w-full mt-2 mb-5">
 
     <UiButton type="button" variant="destructive" class="btn-left ml-5 px-4 py-2"  @click="logout">
         Logout
       </UiButton>
-      
+
       <div class="flex-grow"></div>
-      
-      <div class="flex-grow">
-        <UiInput class="w-full max-w-sm px-4 py-2 border rounded-lg" placeholder="Searching"></UiInput>
-      </div>
-      
-      
-      
-      <div class="flex mr-8 ml-0">
+
+    <div class="flex-grow">
+      <UiInput class="w-full max-w-sm px-4 py-2 border rounded-lg" placeholder="Searching"></UiInput>
+    </div>
+     
+
+    
+    <div class="flex mr-8 ml-0">
         <UiButton variant="primary" class="btn-right text-black px-4 py-4 w-24 border-solid border-2 p-2 border-black mr-3">Create Post</UiButton>
-        
+
         <UiButton variant="primary" class="btn-right text-black px-4 py-4 w-24 border-solid border-2 p-2 border-black">Hello: {{ AStore.myName }}</UiButton>
       </div>
     </div>
-    
-    <div class="main_container flext justify-center mt-3">
-      <div class="cards flex flex-col flex-di items-center">
   
+    <div class="main_container flext justify-center">
+      <div class="cards flex flex-col flex-di items-center ">
+
         <PostTredsCards v-for="post in posts" :key="post.id" :post="post"></PostTredsCards>
       </div>
-
     </div>
-    
-  
-  
   
 </template>
 
