436a35877867f0f0950d424ce984cb94a331e4ed
timmi <timajaf2005@gmail.com>
2024-12-01 20:29:16 +0100

    new fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   ssr:true,
   compatibilityDate: '2024-04-03',
-  nitro: {
-    preset: 'firebase', // Настройка для развертывания на Firebase
-  },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   shadcn: {
     
