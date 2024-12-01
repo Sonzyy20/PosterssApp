54d3b7cbb94b496ee2f3de212bb38ec7c0e9b8a3
Timofiy Kabysh <111361955+Sonzyy20@users.noreply.github.com>
2024-12-01 20:04:30 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr:true,
-  buildDir: 'dist',
   compatibilityDate: '2024-04-03',
   nitro: {
     preset: 'firebase', // Настройка для развертывания на Firebase
